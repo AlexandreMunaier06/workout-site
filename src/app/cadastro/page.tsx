be15f36aabafe1f70b1c10d045774ba93b4c1cd7
AlexandreMunaier06 <alexandremunaier33@gmail.com>
2025-01-29 10:39:51 -0300

    att front login

---
+++ src/app/cadastro/page.tsx
@@ -0,0 +1,7 @@
+function Cadastro() {
+  return (
+    <h1>Pagina de cadastro</h1>
+  )
+}
+
+export default Cadastro;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,26 @@
+import Image from 'next/image';
+import pesos from '../../../public/images/pesos.jpg';
+import './login.css';
+import Link from 'next/link';
+
 function Page() {
   return (
-    <h1>...</h1>
+    <main className="main-container">
+      <section className="login-container">
+        <div className='login'>
+          <form action="" className='forms-login'>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" required/>
+            <label htmlFor="senha">Senha:</label>
+            <input type="password" id="senha" required/>
+            <button>Entrar</button>
+            <Link href='./forgot-password'>Esqueci minha senha</Link>
+            <Link href='./cadastro'>Não tem cadastro? Clique aqui!</Link>
+          </form>
+        </div>
+        <Image src={ pesos } alt="pesos de academia" className='image' />
+      </section>
+    </main>
   )
 }
 
