f31f2b30b00814eb7ec35df54595a3c3ddce5afd
AlexandreMunaier06 <alexandremunaier33@gmail.com>
2025-02-07 19:06:49 -0300

    api cadastro finalizada

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -3,12 +3,67 @@
 import Image from 'next/image';
 import './cadastro.css';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
+
+type FormCadastro = {
+  firstName: string,
+  lastName: string,
+  birthday: string,
+  email: string,
+  password: string,
+  gender: 'masculino' | 'feminino',
+  weight: string,
+  height: string,
+}
 
 function Cadastro() {
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const [formData, setFormData] = useState<FormCadastro>({
+    firstName: '',
+    lastName: '',
+    birthday: '',
+    email: '',
+    password: '',
+    gender: 'masculino',
+    weight: '',
+    height: '',
+  });
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+  
+    const form = {
+      first_name: formData?.firstName,
+      last_name: formData?.lastName,
+      birthday: formData?.birthday,
+      email: formData?.email,
+      password: formData?.password,
+      gender: formData?.gender,
+      weight: formData?.weight ? Number(formData.weight) : null,
+      height: formData?.height ? Number(formData.height) : null
+    };
+  
+    console.log('Enviando:', form);
+  
+    try {
+      const response = await fetch('http://localhost:3001/cadastro/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(form)
+      });
+  
+      const data = await response.json();
+      console.log('Resposta do backend:', data);
+    } catch (err) {
+      console.error('Erro na requisição:', err);
+    }
+  };
+
+  const handleSetForm = (name: string, event: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    setFormData({
+      ...formData,
+      [name]: event.target.value.toString(),
+    })
   }
 
   return (
@@ -17,36 +72,88 @@ function Cadastro() {
         <div className='login'>
           <form action="" className='forms-login' onSubmit={(e) => handleSubmit(e)}>
             <label htmlFor="name">Nome:</label>
-            <input type="text" id="name" required/>
+            <input
+              type="text"
+              id="name"
+              value={formData.firstName}
+              onChange={(event) => handleSetForm('firstName', event)}
+              required
+            />
 
             <label htmlFor="lastName">Sobrenome:</label>
-            <input type="text" id="lastName" required/>
+            <input
+              type="text"
+              id="lastName"
+              value={formData.lastName}
+              onChange={(event) => handleSetForm('lastName', event)}
+              required
+            />
 
             <label htmlFor="nascimento">Data de nascimento:</label>
-            <input type="date" id="nascimento" required/>
+            <input
+              type="date"
+              id="nascimento"
+              value={formData.birthday}
+              onChange={(event) => handleSetForm('birthday', event)}
+              required
+            />
 
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" required/>
+            <input
+              type="email"
+              id="email"
+              value={formData.email}
+              onChange={(event) => handleSetForm('email', event)}
+              required
+            />
 
             <label htmlFor="senha">Senha:</label>
-            <input type="password" id="senha" required/>
+            <input
+              type="password"
+              id="senha"
+              value={formData.password}
+              onChange={(event) => handleSetForm('password', event)}
+              required/>
 
             <label htmlFor="confirmSenha">Confirmar Senha:</label>
-            <input type="password" id="confirmSenha" required/>
+            <input
+              type="password"
+              id="confirmSenha"
+              required
+            />
 
             <label htmlFor="genero">Genero:</label>
-            <select name="genero" id="genero">
+            <select
+              name="genero"
+              id="genero"
+              value={formData.gender}
+              onChange={(event) => handleSetForm('gender', event)}
+            >
               <option value="masculino">Masculino</option>
               <option value="feminino">Feminino</option>
             </select>
 
             <label htmlFor="peso">Peso (kg):</label>
-            <input type="number" id="peso" placeholder='Peso em kilos' required/>
+            <input
+              type="text"
+              id="peso"
+              value={formData.weight}
+              onChange={(event) => handleSetForm('weight', event)}
+              placeholder='Peso em kilos'
+              required
+            />
 
             <label htmlFor="altura">Altura (cm):</label>
-            <input type="number" id="altura" placeholder='Altura em centímetros' required/>
+            <input
+              type="text"
+              id="altura"
+              value={formData.height}
+              onChange={(event) => handleSetForm('height', event)}
+              placeholder='Altura em centímetros'
+              required
+            />
 
-            <button>Cadastrar</button>
+            <button type='submit'>Cadastrar</button>
             <Link href='./login'>Já tem cadastro? Clique aqui!</Link>
           </form>
         </div>
